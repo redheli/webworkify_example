5bae3b5e0c82f2ab7fb49bed6ac250f2e797937d
Max <zhiyong.weng@gmail.com>
2016-05-11 18:21:04 +0800

    add comments

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 'use strict';
 
-var work = require('webworkify');
+var WebWorkify = require('webworkify');
 
-var w = work(require('./worker.js'));
+var w = WebWorkify(require('./worker.js'));
 w.addEventListener('message', function (ev) {
-    console.log((ev.data).toString());
+    console.log('main: '+(ev.data).toString());
     var s = (ev.data).toString()
     $('#output').append(s + "<br/>");
 });
 
+// w send message '4' to worker.js
+// worker.js keep post message '5 xxxx' '6 xxxx '
+// w receive message '5 xxxx' ...
 w.postMessage(4); // send the worker a message
--- js/worker.js
+++ js/worker.js
@@ -2,10 +2,14 @@ var gamma = require('gamma');
 
 module.exports = function (self) {
     self.addEventListener('message',function (ev){
+        console.log('workers: '+ev.data);
         var startNum = parseInt(ev.data); // ev.data=4 from main.js
-        
+
+        var count=0;
         setInterval(function () {
+            console.log('workers setInterval fun');
             var r = startNum / Math.random() - 1;
+            startNum++;
             self.postMessage([ startNum, r, gamma(r) ]);
         }, 500);
     });
